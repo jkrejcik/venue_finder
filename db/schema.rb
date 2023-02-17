4202786a73bb7cd8e939abab345b836b6af91cdf
Ronan OBoyle <ronanoboyle.rob@gmail.com>
2023-02-17 18:59:04 +0000

    bookings table generated

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_17_154547) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_17_185707) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.string "status"
+    t.bigint "venue_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+    t.index ["venue_id"], name: "index_bookings_on_venue_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -32,13 +42,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_17_154547) do
     t.text "description"
     t.integer "capacity"
     t.integer "price"
-    t.date "available_dates"
     t.string "picture_url"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "available_start_date"
+    t.date "available_end_date"
     t.index ["user_id"], name: "index_venues_on_user_id"
   end
 
+  add_foreign_key "bookings", "users"
+  add_foreign_key "bookings", "venues"
   add_foreign_key "venues", "users"
 end
