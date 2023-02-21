04f0b0ade488c768f717519e378d17c1948e9675
Ronan OBoyle <ronanoboyle.rob@gmail.com>
2023-02-21 14:55:56 +0000

    uncommented validation

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -6,7 +6,7 @@ class Venue < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :address, presence: true
   validates :description, length: { minimum: 6 }
-  # validates :capacity, presence: true, numericality: { greater_than_or_equal_to: 10 }
+  validates :capacity, presence: true, numericality: { greater_than_or_equal_to: 10 }
   validates :price, presence: true
   # validates :available_dates, presence: true
 end
